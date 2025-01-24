644ed0a618984569124eeb332fec59a5c148486b
ArjunTiware <at8949681@gmail.com>
2025-01-24 09:26:10 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,17 @@
 import './App.css';
+import { Header, Navbar, Hero, Card, Contact, Footer } from './components';
 
-  return (
-    <>
-      
-    </>
-  )
+  
+  const App = () => {
+    <div className='w-full overflow-hidden scroll-smooth'>
+      <Header />
+      <Navbar />
+      <Hero />
+      <Card />
+      <Contact />
+      <Footer />
+    </div>
+  }
+  
 
 export default App;
