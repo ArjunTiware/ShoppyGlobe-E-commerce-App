9f060282a5d44cd4238d9adff77b89ea42f4af50
ArjunTiware <at8949681@gmail.com>
2025-02-23 19:38:47 +0530

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ const App = () => {
   return (
     <div className='w-full overflow-hidden scroll-smooth'>
       <DashboardLayoutBasic />
-      <Outlet />
+      {/* <Outlet /> */}
     </div>
   );
 }
