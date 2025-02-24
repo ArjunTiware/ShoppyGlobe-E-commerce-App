cbcaa8d914cfd16215d61f6f1e1a59107443c74d
ArjunTiware <at8949681@gmail.com>
2025-02-24 16:28:05 +0530

    deleted all files

--- src/Helper/getPrice.js
+++
@@ -1,5 +0,0 @@
-function getPrice(arr){
-    return  Math.floor(arr.map((e)=>((e.price*e.quantity)*100)/(100-e.discountPercentage)).reduce((acc,curr)=>acc+curr,0))
-  }
-  
-  export default getPrice;
--- src/store/Appstore.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from '@reduxjs/toolkit'
-import CartSlice from "./Cartslice"
-const store = configureStore({
-    reducer: {
-        cart: CartSlice
-    }
-})
-
-export default store;
