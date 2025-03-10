d8d47211a0d0b8713c747e2cc9f1f10a8a0e9296
ArjunTiware <at8949681@gmail.com>
2025-03-10 14:33:34 +0530

    updates

---
+++ Ecommerce/src/components/index.js
@@ -0,0 +1,17 @@
+import Home from './Home';
+import Header from './Header';
+import Cart from './Cart';
+import ProductList from './ProductList';
+import ProductDetails from './ProductDetails';
+import CartItem from './CartItem';
+import ProductItem from './ProductItem';
+
+export {
+    Home,
+    Header,
+    Cart,
+    ProductItem,
+    CartItem,
+    ProductDetails,
+    ProductList
+};
