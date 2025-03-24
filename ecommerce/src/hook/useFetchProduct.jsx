a59501d07e0f186f3dee970b971d516e5243394c
ArjunTiware <at8949681@gmail.com>
2025-03-24 21:43:35 +0530

    updates

--- ecommerce/src/hook/useFetchProduct.jsx
+++ ecommerce/src/hook/useFetchProduct.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-const useProductList = () => {
+const useFetchProduct = () => {
   const [products, setProducts] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
@@ -30,4 +30,4 @@ const useProductList = () => {
   return { products, loading, error };
 }
 
-export default useProductList;
+export default useFetchProduct;
